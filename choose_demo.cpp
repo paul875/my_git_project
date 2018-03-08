1ceda5fe55231a0ed36473bef6c2316d64bde8df
Pavel <Pavel>
2018-03-08 18:20:01 +0300

    Commit first

---
+++ choose_demo.cpp
@@ -0,0 +1,47 @@
+// choose_demo.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Impact_metod.h"
+#include "choose_demo.h"
+
+extern int ch_scenary_demo;
+
+// Cchoose_demo dialog
+
+IMPLEMENT_DYNAMIC(Cchoose_demo, CDialog)
+
+Cchoose_demo::Cchoose_demo(CWnd* pParent /*=NULL*/)
+	: CDialog(Cchoose_demo::IDD, pParent)
+	, m_ch_demo(0)
+{
+
+}
+
+Cchoose_demo::~Cchoose_demo()
+{
+}
+
+void Cchoose_demo::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Radio(pDX, IDC_RADIO1_DEMO, m_ch_demo);
+}
+
+
+BEGIN_MESSAGE_MAP(Cchoose_demo, CDialog)
+	ON_BN_CLICKED(IDOK, &Cchoose_demo::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// Cchoose_demo message handlers
+
+void Cchoose_demo::OnBnClickedOk()
+{
+	// TODO: Add your control notification handler code here
+	OnOK();
+
+	UpdateData(TRUE);
+
+	ch_scenary_demo = m_ch_demo;
+}
---
+++ choose_demo.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+// Cchoose_demo dialog
+
+class Cchoose_demo : public CDialog
+{
+	DECLARE_DYNAMIC(Cchoose_demo)
+
+public:
+	Cchoose_demo(CWnd* pParent = NULL);   // standard constructor
+	virtual ~Cchoose_demo();
+
+// Dialog Data
+	enum { IDD = IDD_DIALOG_DEMO };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_ch_demo;
+	afx_msg void OnBnClickedOk();
+};
